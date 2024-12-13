219b68e58ba3c17141d9bf334b9774de1af7a863
Gaurav <gauravsharma25404@gmail.com>
2024-12-13 19:19:29 +0530

    userData route added

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const express = require('express')
 
 const app = express()
 
-const port = 5000
+// const port = 5000
+const userData = {
+    "Name" : "Gaurav Sharma",
+    "Roll no" : "21ESKCX021",
+    "Branch" : "Data Science",
+    "Age" : 21
+}
 
 app.get('/', (req, res) => {
     res.send("<h2>this is the home page of website</h2>");
@@ -19,6 +25,10 @@ app.get('/register', (req, res) => {
 
 app.get('/main', (req, res) =>{
     res.send("<h1>it is the main page of the website</h1>")
+})
+
+app.get("/userData", (req,res) => {
+    res.json(userData)
 })
 
 app.listen(process.env.PORT, () => {
